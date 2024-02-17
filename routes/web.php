a999a04367880d9b26631c3f36069f4f48715e85
= <zafor.iqbal13@gmail.com>
2024-02-17 19:14:34 +0600

    project setup and home page add in route and view

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/services', function () {
+    return view('services');
+});
+Route::get('/business-planning', function () {
+    return view('business-planning');
+});
+Route::get('/market-research', function () {
+    return view('market-research');
+});
+Route::get('/business-consulting', function () {
+    return view('business-consulting');
+});
